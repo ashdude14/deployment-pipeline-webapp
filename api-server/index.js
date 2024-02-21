f20ea1c23ab04063b94305cc614adf8e145b0b16
unknown <ashish.kumar.singh.jee@gmail.com>
2024-02-22 00:42:53 +0530

    api-server-done

--- api-server/index.js
+++ api-server/index.js
@@ -8,21 +8,51 @@ const PORT=9000;
 app.use(express.json());
 
 const ecsClient=new ECSClient({
+    region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: 'AKIAXXJWDYFACXZK3JFD',
-        secretAccessKey: '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'
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY
     }
 
 })
 
+const config ={
+    Cluster: process.env.ARN_CLUSTER,
+    Task :process.env.ARN_TASK
 
-app.post('/project',(req,res)=>{
+}
+
+app.post('/project',async (req,res)=>{
     const {gitUrl}=req.body;
     const projectID=generateSlug()
     //spin  the container non ecs
     const command= new RunTaskCommand({
+        cluster:config.Cluster,
+        taskDefinition:config.Task,
+        launchType:'FARGATE',
+        count:1,
+        networkConfiguration : {
+            awsvpcConfiguration : {
+              assignPublicIp: "ENABLED",
+              subnets : ['subnet-0c9f50c7e063632d5','subnet-0a12696295a19aca5','subnet-04d711719a6cd6e86'],
+              securityGroups : ['sg-0d349ab544b761770']
+            }
+        },
+        overrides : {
+            containerOverrides : [
+                {
+                    name: " builder-image",
+                    environment : [
+                              { name: "GIT_REPOSITORY__URL",value:gitUrl} ,
+                              {name:"PROJECT_ID",value:projectID} 
+        
+                    ]
+                }
+            ]
          
-    })
+}})
+   await ecsClient.send(command);
+   return res.json({status:'queued',data : {projectID,url:`https://${projectID}.localhost:8000`}})
 })
 
 app.listen(PORT,()=>{console.log(`API Server running on Port: ${PORT}`)})
