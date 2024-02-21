e2f492f2eb17ba64593ce26c0c032f41e10bd971
unknown <ashish.kumar.singh.jee@gmail.com>
2024-02-21 20:11:28 +0530

    build-server

---
+++ build-server/main.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export GIT_REPOSITORY__URL="$GIT_REPOSITORY__URL"
+
+git clone "$GIT_REPOSITORY__URL" /home/app/output
+
+exec node script.js
---
+++ build-server/script.js
@@ -0,0 +1,76 @@
+const { exec } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const mime = require('mime-types');
+
+const s3Client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+        accessKeyId: 'AKIAXXJWDYFACXZK3JFD',
+        secretAccessKey: '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'
+    }
+});
+
+const PROJECT_ID = process.env.PROJECT_ID;
+
+async function init() {
+    console.log('Executing script.js');
+    const outDirPath = path.join(__dirname, 'output');
+
+    // fs.mkdir(outDirPath, { recursive: true }, (err) => {
+    //     if (err) {
+    //         console.error('Error creating directory:', err);
+    //     } else {
+    //         console.log('Directory created successfully:', outDirPath);
+    //     }
+    // });
+
+    const p = exec(`cd ${outDirPath} && npm install && npm run build`);
+
+    p.stdout.on('data', function (data) {
+        console.log(data.toString());
+    });
+
+    p.stderr.on('data', function (data) {
+        console.error('Error:', data.toString());
+    });
+
+    p.on('close', async function () {
+        console.log('Build Complete');
+        //
+        const distFolderPath = path.join(__dirname, 'output', 'dist');;
+        // fs.mkdir(distFolderPath, { recursive: true }, (err) => {
+        //     if (err) {
+        //         console.error('Error creating directory:', err);
+        //     } else {
+        //         console.log('Directory created successfully:', distFolderPath);
+        //     }
+        // });
+        const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true });
+
+        for (const file of distFolderContents) {
+            const filePath = path.join(distFolderPath, file);
+            if (fs.lstatSync(filePath).isDirectory()) continue;
+
+            console.log('uploading', filePath);
+
+            const command = new PutObjectCommand({
+                Bucket: 'vercel-clone-ash',
+                Key: `__outputs/${PROJECT_ID}/${file}`,
+                Body: fs.createReadStream(filePath),
+                ContentType: mime.lookup(filePath)
+            });
+
+            try {
+                await s3Client.send(command);
+                console.log('uploaded', filePath);
+            } catch (error) {
+                console.error('Error uploading file:', error);
+            }
+        }
+        console.log('Done...');
+    });
+}
+
+init();
